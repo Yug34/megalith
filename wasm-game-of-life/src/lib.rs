56d19c1724086ae26624e46e4c9dfe1c19bde987
Yug34 <yug.gjr@gmail.com>
2025-04-13 16:42:47 +0530

    move wasm to public dir for url import

--- wasm-game-of-life/src/lib.rs
+++ wasm-game-of-life/src/lib.rs
@@ -26,6 +26,7 @@ pub struct Universe {
     height: u32,
     cells: Vec<Cell>,
 }
+#[wasm_bindgen]
 impl Universe {
     fn get_index(&self, row: u32, column: u32) -> usize {
         (row * self.width + column) as usize
